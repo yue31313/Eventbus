2a672c187f51f58b5e595b55a6614f27f16974cf
韩亚飞_韩梦飞沙 <313134555@qq.com>
2017-11-16 13:18:33 +0800

    删除无用版本控制 添加忽略

--- app/src/main/java/han_meng_fei_sha/eventbusdemo/MainActivity.kt
+++ app/src/main/java/han_meng_fei_sha/eventbusdemo/MainActivity.kt
@@ -13,20 +13,25 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener({ EventBus.getDefault().post(MessageEvent("主线程发送过来的消息事件")) })
-        button2.setOnClickListener({ EventBus.getDefault().postSticky(NianEvent("粘性消息事件"))})
-        button3.setOnClickListener({if(!EventBus.getDefault().isRegistered(this@MainActivity)){
-            EventBus.getDefault().register(this@MainActivity)
-        }})
+        button2.setOnClickListener({ EventBus.getDefault().postSticky(NianEvent("粘性消息事件")) })
+        button3.setOnClickListener({
+            if (!EventBus.getDefault().isRegistered(this@MainActivity)) {
+                EventBus.getDefault().register(this@MainActivity)
+            }
+        })
     }
+
     override fun onDestroy() {
         super.onDestroy()
         EventBus.getDefault().unregister(this@MainActivity)
     }
+
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun jieShouXiaoXi(messageEvent: MessageEvent) {
         textView.text = messageEvent.name
     }
-    @Subscribe(threadMode = ThreadMode.MAIN,sticky = true)
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     fun jieShouNianXiaoXi(event: NianEvent) {
         textView.text = event.name
     }
